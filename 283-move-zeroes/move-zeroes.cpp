be105bd87f96a810295db8dcb0cb4cfcd32e226d
Aradhya Kulkarni <121120793+aradhya25@users.noreply.github.com>
2026-01-09 22:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 24.7 MB (5.04%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,48 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        //brute force
-        // int n = nums.size();
-        // vector<int> temp;
-
-        // // Store non-zero elements
-        // for(int i = 0; i < n; i++) {
-        //     if(nums[i] != 0) {
-        //         temp.push_back(nums[i]);  // 
-        //     }
-        // }
-
-        // // Overwrite nums with non-zero elements
-        // for(int i = 0; i < temp.size(); i++) {
-        //     nums[i] = temp[i];
-        // }
-
-        // // Fill the rest with zeroes
-        // for(int i = temp.size(); i < n; i++) {
-        //     nums[i] = 0;
-        // }
-
-
-        int n = nums.size();
-        int j = -1;
-
-        // Find the first zero
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                j = i;
-                break;
-            }
+       int n=nums.size();
+       vector<int>temp;
+       //store non zero elements
+       for(int i=0;i<n;i++){
+        if(nums[i]!=0){
+            temp.push_back(nums[i]);
         }
-
-        // If there's no zero, nothing to do
-        if (j == -1) return;
-
-        // Move non-zero elements forward
-        for (int i = j + 1; i < n; i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                j++;
-            }
-        }
+       }
+       for(int i=0;i<temp.size();i++){
+        nums[i]=temp[i];
+       }
+       for(int i=temp.size();i<n;i++){
+        nums[i]=0;
+       }
     }
 };
