c7666bc8b92be997804060a5a3d0301e6588672a
Aradhya Kulkarni <121120793+aradhya25@users.noreply.github.com>
2026-01-09 22:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (54.13%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int n=nums.size();
-       vector<int>temp;
-       //store non zero elements
-       for(int i=0;i<n;i++){
-        if(nums[i]!=0){
-            temp.push_back(nums[i]);
+    //    int n=nums.size();
+    //    vector<int>temp;
+    //    //store non zero elements
+    //    for(int i=0;i<n;i++){
+    //     if(nums[i]!=0){
+    //         temp.push_back(nums[i]);
+    //     }
+    //    }
+    //    for(int i=0;i<temp.size();i++){
+    //     nums[i]=temp[i];
+    //    }
+    //    for(int i=temp.size();i<n;i++){
+    //     nums[i]=0;
+    //    }
+
+    int j=-1;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==0){
+            j=i;
+            break;
         }
-       }
-       for(int i=0;i<temp.size();i++){
-        nums[i]=temp[i];
-       }
-       for(int i=temp.size();i<n;i++){
-        nums[i]=0;
-       }
+    }
+    if(j==-1){
+        return;
+    }
+    for(int i=j+1;i<nums.size();i++){
+        if(nums[i]!=0){
+            swap(nums[i],nums[j]);
+            j++;
+        }
+    }
+
     }
 };
