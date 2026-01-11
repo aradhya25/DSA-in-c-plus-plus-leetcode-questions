c4434b47d7ddaa4a215db0c8196b4a84aef1ccd5
Aradhya Kulkarni <121120793+aradhya25@users.noreply.github.com>
2026-01-11 18:00:19 +0530

    Time: 20 ms (5.42%) | Memory: 138.8 MB (31.19%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -6,16 +6,15 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]>=0){
                 nums2.push_back(nums[i]);
-            }
-            else{
+            }else{
                 nums3.push_back(nums[i]);
             }
         }
-       nums.clear();
-       for(int i=0;i<nums2.size();i++){
-        nums.push_back(nums2[i]);
-        nums.push_back(nums3[i]);
-       }
-       return nums;
+        nums.clear();
+        for(int i=0;i<nums2.size();i++){
+            nums.push_back(nums2[i]);
+            nums.push_back(nums3[i]);
+        }
+        return nums;
     }
 };
