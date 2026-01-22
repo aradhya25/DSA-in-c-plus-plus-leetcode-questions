6d14bd7e5093be98d25ace5afadd07f678e09c09
Aradhya Kulkarni <121120793+aradhya25@users.noreply.github.com>
2026-01-22 18:03:32 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (86.19%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,90 @@
 class Solution {
 public:
+    //     int lowerbound(vector<int>& arr, int n, int x) {
+    //         int low = 0, high = n - 1;
+    //         int ans = n;
+    //         while (low <= high) {
+    //             int mid = (low + high) / 2;
+    //             if (arr[mid] >= x) {
+    //                 ans = mid;
+    //                 high = mid - 1;
+    //             } else {
+    //                 low = mid + 1;
+    //             }
+    //         }
+    //         return ans;
+    //     }
+
+    //    int upperbound(vector<int>& arr, int n, int x) {
+    //     int low = 0, high = n - 1;
+    //     int ans = n;
+    //     while (low <= high) {
+    //         int mid = low + (high - low) / 2;
+    //         if (arr[mid] > x) {
+    //             ans = mid;
+    //             high = mid - 1;
+    //         } else {
+    //             low = mid + 1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        int startpos=-1;
-        int endpos=-1;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                startpos=i;
-                break;
+        // int startpos=-1;
+        // int endpos=-1;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==target){
+        //         startpos=i;
+        //         break;
+        //     }
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     if(nums[i]==target){
+        //         endpos=i;
+        //         break;
+        //     }
+        // }
+        // return{startpos,endpos};
+        int n = nums.size();
+
+        // int lb = lowerbound(nums, n, target);
+        // if (lb == n || nums[lb] != target)
+        //     return {-1, -1};
+        // return {lb, upperbound(nums, n, target) - 1};
+
+        int low = 0;
+        int high = n - 1;
+        int first = -1;
+        int last = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]==target){
-                endpos=i;
-                break;
+        low = 0;
+        high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        return{startpos,endpos};
+        return {first, last};
     }
 };
